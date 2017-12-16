60b9c4c0e08bb06b1f3576ab1cab0ae6b67f22e4
Roger Almeida <roger.eduardo@gmail.com>
2017-12-17 02:35:14 +1100

    Rspec raising errors for deprecations

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -43,6 +43,7 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.raise_errors_for_deprecations!
   config.default_formatter = "doc"
 
 # The settings below are suggested to provide a good initial experience
