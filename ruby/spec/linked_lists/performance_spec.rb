da1c9a7863c76f5808cf35806a4fbd60502bdc84
Roger Almeida <roger.eduardo@gmail.com>
2017-12-18 19:42:54 +1100

    Running performance tests for the 3 implementations of linked list

--- ruby/spec/linked_lists/performance_spec.rb
+++ ruby/spec/linked_lists/performance_spec.rb
@@ -2,25 +2,24 @@
 
 require_relative '../../linked_lists/singly_head_pointer_linked_list'
 require_relative '../../linked_lists/singly_head_and_tail_pointer_linked_list'
+require_relative '../../linked_lists/doubly_linked_list'
 require 'benchmark'
 
-RSpec.describe 'Performance for Linked Lists' do
-  it 'should compare the performance to search a integer in a million long list' do
-    head = SinglyHeadPointerLinkedList.new
-    tail = SinglyHeadAndTailPointerLinkedList.new
-    Benchmark.bm do |bm|
-      bm.report('Inserting a million items in head list') do
-        10000.times { head.push_back Random.rand(10000000) }
+[SinglyHeadPointerLinkedList, SinglyHeadAndTailPointerLinkedList, DoublyLinkedList].each do |clazz|
+  RSpec.describe "Performance for class '#{clazz}" do
+    before(:each) do
+      @list = clazz.new
+    end
+    it 'should compare the performance to search a integer in a million long list' do
+      Benchmark.bm do |bm|
+        bm.report('Inserting a million items in head list') do
+          10000.times { @list.push_back Random.rand(10000000) }
+        end
+        bm.report('list.reverse'){ @list.reverse }
+        bm.report('list.push_back'){ @list.push_back(99) }
+        bm.report('list.pop_back'){ @list.pop_back }
+        bm.report('list.erase'){ @list.erase Random.rand(99) }
       end
-      bm.report('Inserting a million items in tail list') do
-        10000.times { tail.push_back Random.rand(10000000) }
-      end
-      bm.report('Reversing head list'){ head.reverse }
-      bm.report('Reversing tail list'){ tail.reverse }
-      bm.report('head.append'){head.push_back(99)}
-      bm.report('tail.append'){tail.push_back(99)}
-      bm.report('head.pop_back'){head.pop_back}
-      bm.report('tail.pop_back'){tail.pop_back}
     end
   end
 end
